a71b3d5f9203d112b49c4535eb4809e3cb975ce0
Baemint2 <qofndlwl@gmail.com>
2024-05-20 16:21:03 +0900

    이미지 설정 추가

--- src/main/java/com/mcp/crispy/common/config/WebConfig.java
+++ src/main/java/com/mcp/crispy/common/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.mcp.crispy.common.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +13,17 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addRedirectViewController("/", "/crispy/main");
         registry.addRedirectViewController("/crispy", "/crispy/main");
         registry.addRedirectViewController("/crispy/", "/crispy/main");
+        registry.addRedirectViewController("/CRISPY", "/crispy/main");
+        registry.addRedirectViewController("/CRISPY/", "/crispy/main");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/upload/**")
+                .addResourceLocations("file:///Users/baeyeong-ug/Desktop/image/signatures/");
+        registry.addResourceHandler("/profiles/**")
+                .addResourceLocations("file:///Users/baeyeong-ug/Desktop/image/profiles/");
+        registry.addResourceHandler("/franchise/**")
+                .addResourceLocations("file:///Users/baeyeong-ug/Desktop/image/profiles/");
     }
 }
