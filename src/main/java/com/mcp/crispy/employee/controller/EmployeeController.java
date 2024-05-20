d72ed36b8d8868de4bb3352994de10cf2cc953ea
Baemint2 <qofndlwl@gmail.com>
2024-05-20 16:19:38 +0900

    각종 정보 수정 및 정보 찾기 추가

--- src/main/java/com/mcp/crispy/employee/controller/EmployeeController.java
+++ src/main/java/com/mcp/crispy/employee/controller/EmployeeController.java
@@ -1,22 +1,24 @@
 package com.mcp.crispy.employee.controller;
 
+import com.mcp.crispy.employee.dto.EmployeeDto;
+import com.mcp.crispy.employee.service.EmployeeService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
+@Slf4j
 @Controller
 @RequestMapping("/crispy/employee")
+@RequiredArgsConstructor
 public class EmployeeController {
 
-	@GetMapping("/signup")
-	public String signup() {
-		return "employee/signup";
-	}
-
-	@GetMapping("/login")
-	public String login() {
-		return "employee/login";
-	}
+	private final EmployeeService employeeService;
 
 	@GetMapping("/find/username")
 	public String findUsername() {
@@ -29,7 +31,16 @@ public class EmployeeController {
 	}
 
 	@GetMapping("/change/password")
-	public String changePassword() {
+	public String changePassword(HttpSession session, Principal principal, Model model) {
+		if(principal != null) {
+			EmployeeDto employee = employeeService.getEmployeeName(principal.getName());
+			model.addAttribute("empId", employee.getEmpId());
+		} else if (session.getAttribute("empId") != null) {
+			String findEmpId = (String) session.getAttribute("empId");
+			model.addAttribute("empId", findEmpId);
+		} else {
+			return "redirect:/login";
+		}
 		return "employee/change-password";
 	}
 
@@ -38,9 +49,14 @@ public class EmployeeController {
 		return "employee/find-username-result";
 	}
 
-	@GetMapping("/mypage")
-	public String getEmployee() {
-		return "employee/employee";
+	// 직원 혹은 관리자 개인이 들어가는 마이 페이지
+	@GetMapping("/profile")
+	public String getEmployee(Principal principal, Model model) {
+		EmployeeDto employee = employeeService.getEmployeeName(principal.getName());
+		log.info("address : {} {} {}", employee.getEmpZip(), employee.getEmpStreet(), employee.getEmpDetail());
+		log.info("empSign : {}", employee.getEmpSign());
+		model.addAttribute("employee", employee);
+		return "employee/employee-profile";
 	}
 
 }
