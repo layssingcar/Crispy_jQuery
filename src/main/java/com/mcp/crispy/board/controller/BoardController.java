05d78bf6fffff314b6a3ca553976e31fd7f989ac
ohjeong2222 <jeoh6006@gmail.com>
2024-05-20 14:48:40 +0900

    0520

---
+++ src/main/java/com/mcp/crispy/board/controller/BoardController.java
@@ -0,0 +1,34 @@
+package com.mcp.crispy.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	
+	@GetMapping("/board-list")
+	public String boardList() {
+		return "board/board-list";
+	}
+	@GetMapping("/board-list2")
+	public String boardList2() {
+		return "board/board-list2";
+	}
+	@GetMapping("/board-add")
+	public String boardAdd() {
+		return "board/board-add";
+	}
+	@GetMapping("/board-modify")
+	public String boardModify() {
+		return "board/board-modify";
+	}
+	@GetMapping("/board-detail")
+	public String boardDetail() {
+		return "board/board-detail";
+	}
+
+	
+	
+}
