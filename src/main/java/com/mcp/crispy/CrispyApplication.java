16937b749936841625d9d23482b02c5c161ccdfa
Baemint2 <qofndlwl@gmail.com>
2024-05-20 16:04:38 +0900

    @EnableAsync로 수정

--- src/main/java/com/mcp/crispy/CrispyApplication.java
+++ src/main/java/com/mcp/crispy/CrispyApplication.java
@@ -5,9 +5,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Async
+@EnableAsync
 @EnableScheduling
 @MapperScan("com.mcp.crispy.*")
 @SpringBootApplication
