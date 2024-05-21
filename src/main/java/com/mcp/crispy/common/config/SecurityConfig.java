565c2a2b9d18ddf60171657913616d12ad2da550
Baemint2 <qofndlwl@gmail.com>
2024-05-21 16:11:56 +0900

    패키지명 수정

--- src/main/java/com/mcp/crispy/common/config/SecurityConfig.java
+++ src/main/java/com/mcp/crispy/common/config/SecurityConfig.java
@@ -1,6 +1,6 @@
-package com.mcp.crispy;
+package com.mcp.crispy.common.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,9 +14,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
