8385b21ee6fc143dc467eb6861324b2cadbf5428
Baemint2 <qofndlwl@gmail.com>
2024-05-22 06:37:33 +0900

    관리자 계정 등록용 테스트코드

--- src/main/java/com/mcp/crispy/admin/mapper/AdminMapper.java
+++ src/main/java/com/mcp/crispy/admin/mapper/AdminMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface AdminMapper {
 
-//    void insertAdmin(AdminDto adminDto);
+    void insertAdmin(AdminDto adminDto);
 
     @Select("SELECT ADMIN_ID, ADMIN_PW FROM ADMIN_T WHERE ADMIN_ID = #{adminId}")
     AdminDto findByUsername(String username);
