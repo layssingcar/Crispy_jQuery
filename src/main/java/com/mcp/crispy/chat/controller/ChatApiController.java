a5adc1352d480cdd674184fb7f1b36afed81acdc
Baemint2 <qofndlwl@gmail.com>
2024-05-24 23:37:53 +0900

    채팅방 생성시 초대한 멤버도 CR_EMP_T 테이블에 추가, 접속기록, 입장기록 추가

--- src/main/java/com/mcp/crispy/chat/controller/ChatApiController.java
+++ src/main/java/com/mcp/crispy/chat/controller/ChatApiController.java
@@ -16,6 +16,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -29,16 +30,14 @@ public class ChatApiController {
 
     @GetMapping("/rooms/v1")
     public List<ChatRoomDto> getChatRooms(Authentication authentication) {
-            CustomDetails userDetails = (CustomDetails) authentication.getPrincipal();
-            int empNo = userDetails.getEmpNo();
+        CustomDetails userDetails = (CustomDetails) authentication.getPrincipal();
 
-        return chatService.getChatRooms(empNo);
+        return chatService.getChatRooms(userDetails.getEmpNo());
     }
 
     @GetMapping("/rooms/{chatRoomNo}/v1")
-    public ChatRoomDto getChatRoom(@PathVariable Integer chatRoomNo, Authentication authentication) {
-        CustomDetails userDetails = (CustomDetails) authentication.getPrincipal();
-        return chatService.getChatRoom(chatRoomNo, userDetails.getEmpNo());
+    public ChatRoomDto getChatRoom(@PathVariable Integer chatRoomNo) {
+        return chatService.getChatRoom(chatRoomNo);
     }
 
     @GetMapping("/rooms/{chatRoomNo}/messages/v1")
@@ -50,9 +49,18 @@ public class ChatApiController {
     @PostMapping("/rooms/{creatorEmpNo}/v1")
     public ResponseEntity<?> createChatRoom(@RequestBody ChatRoomDto chatRoom, @PathVariable Integer creatorEmpNo) {
         log.info("채팅방 생성: {} {} {}", chatRoom.getChatRoomNo(), chatRoom.getChatRoomTitle(), chatRoom.getCreator());
-        chatService.createChatRoom(chatRoom, creatorEmpNo);
+        Integer chatRoomNo = chatService.createChatRoom(chatRoom, creatorEmpNo);
         log.info("생성자 번호 : {}", creatorEmpNo);
-        return ResponseEntity.ok().build();
+
+        if(chatRoom.getParticipants() != null && !chatRoom.getParticipants().isEmpty()) {
+            for(CrEmpDto participant : chatRoom.getParticipants()) {
+                log.info("participant: {}", participant.toString());
+                participant.setChatRoomNo(chatRoomNo);
+                chatService.addParticipant(participant);
+            }
+        }
+        log.info("채팅방 생성 및 참가자 추가 완료: {}", chatRoomNo);
+        return ResponseEntity.ok().body(Map.of("message","채팅방 생성 및 초대 완료, 방 번호: " + chatRoomNo));
     }
 
     @PostMapping("/rooms/{chatRoomNo}/invite/v1")
@@ -68,5 +76,26 @@ public class ChatApiController {
         chatService.saveMessage(message);
         log.info("메소드 호출 테스트");
         return message;
+    }
+
+    @PostMapping("/rooms/{chatRoomNo}/access/v1")
+    public ResponseEntity<Void> addAccessRecord(@PathVariable Integer chatRoomNo, Authentication authentication) {
+        CustomDetails userDetails = (CustomDetails) authentication.getPrincipal();
+        chatService.handleAccess(chatRoomNo, userDetails.getEmpNo());
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/rooms/{chatRoomNo}/entry")
+    public ResponseEntity<Void> handleEntryRecord(@PathVariable Integer chatRoomNo, Authentication authentication) {
+        CustomDetails userDetails = (CustomDetails) authentication.getPrincipal();
+        chatService.handleEntryRecord(chatRoomNo, userDetails.getEmpNo());
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/rooms/{chatRoomNo}/exit")
+    public ResponseEntity<Void> handleExitRecord(@PathVariable Integer chatRoomNo, Authentication authentication) {
+        CustomDetails userDetails = (CustomDetails) authentication.getPrincipal();
+        chatService.handleExitRecord(chatRoomNo, userDetails.getEmpNo());
+        return ResponseEntity.ok().build();
     }
 }
