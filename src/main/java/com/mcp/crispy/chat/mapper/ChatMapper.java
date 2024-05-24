0955bc13de0c2f0bfbb88d716a01faca15c468fc
Baemint2 <qofndlwl@gmail.com>
2024-05-24 23:32:52 +0900

    채팅방 접속 기록 및 입장 기록 추가 ( 삽입, 수정 )

--- src/main/java/com/mcp/crispy/chat/mapper/ChatMapper.java
+++ src/main/java/com/mcp/crispy/chat/mapper/ChatMapper.java
@@ -4,16 +4,44 @@ import com.mcp.crispy.chat.dto.ChatMessageDto;
 import com.mcp.crispy.chat.dto.ChatRoomDto;
 import com.mcp.crispy.chat.dto.CrEmpDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface ChatMapper {
-    List<ChatRoomDto> getChatRooms(Integer empNo);
+    // 채팅방 목록
+    List<ChatRoomDto> getChatRooms(@Param("empNo")Integer empNo);
+
+    // 채팅방 내부 ( 참가자 )
     ChatRoomDto getChatRoom(Integer chatRoomNo);
+
+    // 채팅방 내에 메시지 목록
     List<ChatMessageDto> getMessages(Integer chatRoomNo);
+
+    // 메시지 저장
     void saveMessage(ChatMessageDto chatMessageDto);
+
+    // 채팅방 만들기
     void createChatRoom(ChatRoomDto chatRoomDto);
+
+    // 유저 초대
     void addParticipant(CrEmpDto crEmpDto);
     List<CrEmpDto> getParticipantsByRoom(Integer chatRoomNo);
+
+    // 채팅방 접속 기록 (최초 삽입, 이후 수정)
+    void addAccessRecord(@Param("chatRoomNo") Integer chatRoomNo, @Param("empNo") Integer empNo);
+    void updateAccessRecord(@Param("chatRoomNo") Integer chatRoomNo, @Param("empNo") Integer empNo);
+    // 접속 기록 체크
+    Integer checkAccessExists(@Param("chatRoomNo") Integer chatRoomNo, @Param("empNo") Integer empNo);
+
+    // 입장 기록
+    void insertEntryRecord(@Param("chatRoomNo") Integer chatRoomNo, @Param("empNo") Integer empNo);
+    void updateExitRecord(@Param("chatRoomNo") Integer chatRoomNo, @Param("empNo") Integer empNo);
+    void updateEntryRecord(@Param("chatRoomNo") Integer chatRoomNo, @Param("empNo") Integer empNo);
+    Integer checkEntryRecordExists(@Param("chatRoomNo") Integer chatRoomNo, @Param("empNo") Integer empNo);
+
+
+
+
 }
