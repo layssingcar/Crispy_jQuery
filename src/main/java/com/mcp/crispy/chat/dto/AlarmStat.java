dddf1c971953496caf9cb7ef2e99f098fc19177a
Baemint2 <qofndlwl@gmail.com>
2024-05-28 08:58:43 +0900

    AlarmStat, EntryStat TypeHandler 추가

--- src/main/java/com/mcp/crispy/chat/dto/AlarmStat.java
+++ src/main/java/com/mcp/crispy/chat/dto/AlarmStat.java
@@ -10,4 +10,13 @@ public enum AlarmStat {
     INACTIVE(1); //입장 비활성화 상태
 
     private final int status;
+
+    public static AlarmStat of(int status) {
+        for (AlarmStat stat : AlarmStat.values()) {
+            if (stat.status == status) {
+                return stat;
+            }
+        }
+        throw new IllegalArgumentException("올바르지 못한 상태입니다.: " + status);
+    }
 }
