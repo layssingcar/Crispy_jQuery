046f9bfe92d5bba00703af29924313ee7e682f07
jjonggu <whdrnal33@kakao.com>
2024-05-31 14:33:04 +0900

    지도 패키지 분리

--- src/main/java/com/mcp/crispy/map/mapper/MapMapper.java
+++ src/main/java/com/mcp/crispy/map/mapper/MapMapper.java
@@ -1,4 +1,7 @@
 package com.mcp.crispy.map.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface MapMapper {
 }
--- src/main/java/com/mcp/crispy/map/service/MapService.java
+++ src/main/java/com/mcp/crispy/map/service/MapService.java
@@ -1,4 +1,12 @@
 package com.mcp.crispy.map.service;
 
+import com.mcp.crispy.map.mapper.MapMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MapService {
+
+    private final MapMapper mapMapper;
 }
