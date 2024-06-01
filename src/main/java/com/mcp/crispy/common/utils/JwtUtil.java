ab61220ca7d5d76d7ff5fc0738d3c179289a6ee6
Baemint2 <qofndlwl@gmail.com>
2024-06-01 15:58:23 +0900

    JwtUtil(AccessToken, RefreshToken 생성, 검증) 추가

---
+++ src/main/java/com/mcp/crispy/common/utils/JwtUtil.java
@@ -0,0 +1,109 @@
+package com.mcp.crispy.common.utils;
+
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+@Getter
+@PropertySource("classpath:jwt.properties")
+public class JwtUtil {
+
+    public static final String ISSUER = "moz1mozi.com";
+    public static final int EXP = 900000; //15분
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER = "Authorization";
+
+    private SecretKey key;
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+    }
+
+    public String createAccessToken(UserDetails userDetails) {
+        String username = userDetails.getUsername();
+        log.info("createAccessToken : {}", username);
+        String authorities = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        return Jwts.builder()
+                .issuer(ISSUER)
+                .subject(username) // empId
+                .expiration(new Date(System.currentTimeMillis() + EXP))
+                .claim("username", username)
+                .claim("roles", authorities)
+                .signWith(key)
+                .compact();
+    }
+
+    public String createRefreshToken(UserDetails userDetails) {
+        String username = userDetails.getUsername();
+        log.info("createRefreshToken : {}", username);
+        long refreshExp = 7 * 24 * 60 * 60 * 1000; // 7일
+
+        return Jwts.builder()
+                .issuer(ISSUER)
+                .subject(username) // empId
+                .expiration(new Date(System.currentTimeMillis() + refreshExp))
+                .signWith(key)
+                .compact();
+    }
+
+    public Claims verify(String token) {
+        try {
+            return Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token.replace(TOKEN_PREFIX, ""))
+                    .getPayload();
+        } catch (JwtException | IllegalArgumentException e) {
+            log.error("Invalid JWT token: {}", e.getMessage());
+            return null;
+        }
+    }
+
+    public String getUsernameFromToken(String token) {
+        Claims claims = verify(token);
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            verify(authToken);
+            return true;
+        } catch (SignatureException ex) {
+            log.error("Invalid JWT signature");
+        } catch (MalformedJwtException ex) {
+            log.error("Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            log.error("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            log.error("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            log.error("JWT claims string is empty.");
+        }
+        return false;
+    }
+
+}
