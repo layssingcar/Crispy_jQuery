fdf4e83dfc79b43dcb03f72b7cdaa5fd6ecf07fa
hyed0ni <layssing10955@gmail.com>
2024-06-02 04:48:58 +0900

    JS 함수 순서 및 라인 정리

--- src/main/resources/static/js/stock/stockList.js
+++ src/main/resources/static/js/stock/stockList.js
@@ -3,26 +3,10 @@ const btnClose = document.querySelector(".btn-close");  // 닫기 버튼
 
 // 페이지네이션 옵션 객체
 const optionObj = {
-    "pageNo": 1,
-    "stockCtNo": 0,
-    "sortKey": "stockName",
-    "sortOrder": "ASC"
-};
-
-// 팝업 레이어 닫기
-btnClose.addEventListener("click", () =>
-    modal.style.display = "none");
-
-// 재고 상세 조회
-const fetchStockDetail = async (stockNo) => {
-    const response = await fetch(`/crispy/stock-detail?stockNo=${stockNo}`);
-    const result = await response.json();
-
-    document.querySelector(".modal-title").innerHTML = result.stockName;                        // 재고명
-    document.querySelector(".stock-price > span:first-child").innerHTML = result.stockPrice;    // 재고단가
-    document.querySelector(".stock-price > span:last-child").innerHTML = result.stockUnit;      // 재고단위
-    document.querySelector(".stock-detail").innerHTML = result.stockDetail;                     // 재고이미지
-    document.querySelector(".stock-img").src = result.stockImg;                                 // 재고설명
+    "pageNo": 1,            // 페이지번호
+    "stockCtNo": 0,         // 카테고리번호
+    "sortKey": "stockName", // 정렬기준
+    "sortOrder": "ASC",     // 정렬순서
 };
 
 // 재고 항목
@@ -35,7 +19,7 @@ const addStockRowEventFn = () => {
         stockRow.addEventListener("click", async (e) => {
             modal.style.display = "block"; // 팝업 레이어 열기
             const stockNo = stockRow.dataset.stockNo;
-            await fetchStockDetail(stockNo);
+            await fetchStockDetailFn(stockNo);
         })
     })
 
@@ -46,6 +30,10 @@ const addStockRowEventFn = () => {
         })
     })
 }
+
+// 팝업 레이어 닫기
+btnClose.addEventListener("click", () =>
+    modal.style.display = "none");
 
 // 페이지 이동
 const addPageLinkEventFn = pageNo => {
@@ -66,23 +54,6 @@ const addPageLinkEventFn = pageNo => {
     })
 }
 
-// 재고 항목 리스트
-const getStockItemsFn = async (optionObj) => {
-    const params = new URLSearchParams(); // URL 쿼리 문자열 객체
-
-    for (let key in optionObj)
-        params.append(key, optionObj[key])
-
-    const response = await fetch(`/crispy/stock-items?${params.toString()}`);
-    const html = await response.text();
-    document.querySelector(".stock-list-container").outerHTML = html;
-
-    // 이벤트 재추가
-    addStockRowEventFn();
-    addPageLinkEventFn(optionObj.pageNo === undefined ? 1 : optionObj.pageNo);
-    addSortEventFn();
-}
-
 // 카테고리 구분 조회
 document.querySelector("#stock-ct").addEventListener("change", e => {
     optionObj["stockCtNo"] = e.target.value;
@@ -98,12 +69,10 @@ const addSortEventFn = () => {
     document.querySelector("#stock-name-sort").addEventListener("click", e => {
         if (optionObj["sortKey"] === "stockName")
             optionObj["sortOrder"] = (optionObj["sortOrder"] === "ASC") ? "DESC" : "ASC";
-
         else {
             optionObj["sortKey"] = "stockName";
             optionObj["sortOrder"] = "ASC";
         }
-
         getStockItemsFn(optionObj);
     })
 
@@ -111,14 +80,41 @@ const addSortEventFn = () => {
     document.querySelector("#is-count-sort").addEventListener("click", e => {
         if (optionObj["sortKey"] === "isCount")
             optionObj["sortOrder"] = (optionObj["sortOrder"] === "ASC") ? "DESC" : "ASC";
-
         else {
             optionObj["sortKey"] = "isCount";
             optionObj["sortOrder"] = "ASC";
         }
-
         getStockItemsFn(optionObj);
     })
+}
+
+// 재고 상세 조회
+const fetchStockDetailFn = async (stockNo) => {
+    const response = await fetch(`/crispy/stock-detail?stockNo=${stockNo}`);
+    const result = await response.json();
+
+    document.querySelector(".modal-title").innerHTML = result.stockName;                        // 재고명
+    document.querySelector(".stock-price > span:first-child").innerHTML = result.stockPrice;    // 재고단가
+    document.querySelector(".stock-price > span:last-child").innerHTML = result.stockUnit;      // 재고단위
+    document.querySelector(".stock-detail").innerHTML = result.stockDetail;                     // 재고이미지
+    document.querySelector(".stock-img").src = result.stockImg;                                 // 재고설명
+};
+
+// 재고 항목 리스트
+const getStockItemsFn = async (optionObj) => {
+    const params = new URLSearchParams(); // URL 쿼리 문자열 객체
+
+    for (let key in optionObj)
+        params.append(key, optionObj[key])
+
+    const response = await fetch(`/crispy/stock-items?${params.toString()}`);
+    const html = await response.text();
+    document.querySelector(".stock-list-container").outerHTML = html;
+
+    // 이벤트 재추가
+    addStockRowEventFn();
+    addPageLinkEventFn(optionObj.pageNo === undefined ? 1 : optionObj.pageNo);
+    addSortEventFn();
 }
 
 // 초기화
