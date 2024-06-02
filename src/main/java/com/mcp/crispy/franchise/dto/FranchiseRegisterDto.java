867c851bc6ebb80ad75d2aea2a2a15eff09d4ba1
Baemint2 <qofndlwl@gmail.com>
2024-06-02 23:52:03 +0900

    가맹점 DTO 리팩토링, 주석추가, 코드 리팩토링

---
+++ src/main/java/com/mcp/crispy/franchise/dto/FranchiseRegisterDto.java
@@ -0,0 +1,50 @@
+package com.mcp.crispy.franchise.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FranchiseRegisterDto {
+    private int frnNo;
+
+    @NotBlank(message = "가맹점 이름을 입력하세요.")
+    @Pattern(regexp = "^[가-힣a-zA-Z\\s]+$", message = "가맹점 이름에는 숫자를 포함할 수 없습니다.")
+    private String frnName;
+
+    @NotBlank(message = "대표자 이름을 입력하세요.")
+    @Pattern(regexp = "^[가-힣a-zA-Z\\s]+$", message = "대표자 이름에는 숫자를 포함할 수 없습니다.")
+    private String frnOwner;
+
+    @NotBlank(message = "전화번호를 입력하세요.")
+    @Pattern(regexp = "^\\d{2,3}\\d{3,4}\\d{4}$", message = "올바른 전화번호 형식을 입력해주세요.")
+    private String frnTel;
+
+    @NotBlank(message = "우편번호를 확인해주세요.")
+    @Size(min = 5, max = 5, message = "우편번호는 정확히 5자리여야 합니다.")
+    private String frnZip;
+
+    @NotBlank(message = "도로명 주소를 입력하세요.")
+    private String frnStreet;
+
+    @NotBlank(message = "상세 주소를 입력하세요.")
+    private String frnDetail;
+
+    private String frnStartTime;
+    private String frnEndTime;
+    private LocalDateTime frnJoinDt;
+    private Date createDt;
+    private Date modifyDt;
+    private int creator;
+    private int modifier;
+}
--- src/main/java/com/mcp/crispy/franchise/service/FranchiseService.java
+++ src/main/java/com/mcp/crispy/franchise/service/FranchiseService.java
@@ -2,10 +2,10 @@ package com.mcp.crispy.franchise.service;
 
 import com.mcp.crispy.common.ImageService;
 import com.mcp.crispy.employee.dto.OwnerRegisterDto;
+import com.mcp.crispy.employee.service.EmployeeService;
 import com.mcp.crispy.employee.service.OwnerService;
 import com.mcp.crispy.franchise.dto.FranchiseDto;
-import com.mcp.crispy.franchise.dto.FranchiseImgDto;
-import com.mcp.crispy.franchise.dto.FrnAddressDto;
+import com.mcp.crispy.franchise.dto.FranchiseRegisterDto;
 import com.mcp.crispy.franchise.dto.FrnUpdateDto;
 import com.mcp.crispy.franchise.mapper.FranchiseMapper;
 import lombok.RequiredArgsConstructor;
@@ -16,9 +16,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 
 @Slf4j
@@ -27,34 +24,31 @@ import java.util.List;
 public class FranchiseService {
 
     private final FranchiseMapper franchiseMapper;
-
+    private final EmployeeService employeeService;
     private final OwnerService ownerService;
     private final ImageService imageService;
 
-    /**
-     * 가맹점 및 점주 등록
-     *  2024.05.15
-     */
+    // 가맹점 및 점주 등록
     @Async
     @Transactional
-    public void registerFranchiseAndOwner(FranchiseDto franchiseDto, OwnerRegisterDto ownerRegisterDto) {
-        int frnNo = registerFranchise(franchiseDto);
+    public void registerFranchiseAndOwner(FranchiseRegisterDto franchiseRegisterDto, OwnerRegisterDto ownerRegisterDto) {
+        int frnNo = registerFranchise(franchiseRegisterDto);
         log.info("Franchise registered with no {} ", frnNo);
-        ownerService.registerOwner(ownerRegisterDto, frnNo, franchiseDto.getFrnOwner());
+        ownerService.registerOwner(ownerRegisterDto, frnNo, franchiseRegisterDto.getFrnOwner());
     }
 
+    // 가맹점 등록
     @Transactional
-    public int registerFranchise(FranchiseDto franchiseDto) {
-        FranchiseDto franchise = FranchiseDto.builder()
-                .frnName(franchiseDto.getFrnName())
-                .frnOwner(franchiseDto.getFrnOwner())
-                .frnTel(franchiseDto.getFrnTel())
-                .frnZip(franchiseDto.getFrnZip())
-                .frnStreet(franchiseDto.getFrnStreet())
-                .frnDetail(franchiseDto.getFrnDetail())
-                .frnStartTime(franchiseDto.getFrnStartTime())
-                .frnEndTime(franchiseDto.getFrnEndTime())
-                .frnJoinDt(LocalDateTime.now())
+    public int registerFranchise(FranchiseRegisterDto franchiseRegisterDto) {
+        FranchiseRegisterDto franchise = FranchiseRegisterDto.builder()
+                .frnName(franchiseRegisterDto.getFrnName())
+                .frnOwner(franchiseRegisterDto.getFrnOwner())
+                .frnTel(franchiseRegisterDto.getFrnTel())
+                .frnZip(franchiseRegisterDto.getFrnZip())
+                .frnStreet(franchiseRegisterDto.getFrnStreet())
+                .frnDetail(franchiseRegisterDto.getFrnDetail())
+                .frnStartTime(franchiseRegisterDto.getFrnStartTime())
+                .frnEndTime(franchiseRegisterDto.getFrnEndTime())
                 .build();
 
         franchiseMapper.insertFranchise(franchise);
@@ -66,22 +60,7 @@ public class FranchiseService {
          return franchiseMapper.findByEmpId(empId);
     }
 
-
-    @Transactional
-    public void insertOrUpdateFrnImg(Integer frnNo,
-                                         MultipartFile file) throws IOException {
-        if(file != null && !file.isEmpty()) {
-            String storedProfileImage = imageService.storeProfileImage(file);
-            String storedUrl = "/franchise/" + storedProfileImage;
-            FranchiseImgDto franchiseImgDto = FranchiseImgDto.builder()
-                    .frnNo(frnNo)
-                    .frnImg(storedUrl)
-                    .modifyDt(Date.from(Instant.now()))
-                    .build();
-            franchiseMapper.insertOrUpdateFrnImg(franchiseImgDto);
-        }
-    }
-
+    // 가맹점 번호 수정
     @Transactional
     public void changeFrnTel(String frnTel, Integer frnNo, Integer modifier) {
         if (frnNo == null) {
@@ -90,21 +69,62 @@ public class FranchiseService {
         franchiseMapper.updateFrnTel(frnTel, frnNo, modifier);
     }
 
+    // 가맹점 사진 수정
     @Transactional
-    public void insertOrUpdateFrnAddress(FrnAddressDto frnAddressDto, Integer modifier) {
-        franchiseMapper.insertOrUpdateFrnAddress(frnAddressDto, modifier);
+    public void updateFrnImg(Integer frnNo,
+                             MultipartFile file, Integer modifier) throws IOException {
+        int count = franchiseMapper.countByFrnNo(frnNo);
+
+        if (file == null || file.isEmpty()) {
+            throw new IllegalArgumentException("가맹점 이미지 파일이 없습니다.");
+        } else {
+            String storedProfileImage = imageService.storeProfileImage(file);
+            String storedUrl = "/franchise/" + storedProfileImage;
+
+            if (count > 0) {
+                franchiseMapper.updateFrnImg(storedUrl, frnNo, modifier);
+            } else {
+                throw new IllegalArgumentException("가맹점이 존재하지 않습니다.");
+            }
+        }
+    }
+
+    // 주소 변경
+    @Transactional
+    public void updateFrnAddress(FrnUpdateDto frnUpdateDto, Integer modifier) {
+        int frnNo = frnUpdateDto.getFrnNo();
+        int count = franchiseMapper.countByFrnNo(frnNo);
+
+        if (count > 0) {
+            franchiseMapper.updateFrnAddress(frnUpdateDto, modifier);
+        } else {
+            throw new IllegalArgumentException("가맹점이 존재하지 않습니다.");
+        }
     }
 
     // 대표자명 변경
     @Transactional
-    public void changeFrnOwner(String frnOwner, Integer modifier, Integer frnNo) {
-        franchiseMapper.updateFrnOwner(frnOwner, modifier, frnNo);
+    public void changeFrnOwner(String frnOwner, Integer empNo, Integer modifier, Integer frnNo) {
+        int count = franchiseMapper.countByFrnNo(frnNo);
+        if (count > 0) {
+            employeeService.changeEmpName(frnOwner, empNo, modifier);
+            franchiseMapper.updateFrnOwner(frnOwner, frnNo, modifier);
+        } else {
+            throw new IllegalArgumentException("가맹점이 존재하지 않습니다.");
+        }
     }
 
     // 운영시간 변경
     @Transactional
     public void changeOperatingTime(FrnUpdateDto frnUpdateDto, Integer modifier) {
-        franchiseMapper.updateOperatingTime(frnUpdateDto.getFrnStartTime(), frnUpdateDto.getFrnEndTime(), modifier, frnUpdateDto.getFrnNo());
+        Integer frnNo = frnUpdateDto.getFrnNo();
+        int count = franchiseMapper.countByFrnNo(frnNo);
+
+        if (count > 0) {
+            franchiseMapper.updateOperatingTime(frnUpdateDto.getFrnStartTime(), frnUpdateDto.getFrnEndTime(), modifier, frnUpdateDto.getFrnNo());
+        } else {
+            throw new IllegalArgumentException("가맹점이 존재하지 않습니다.");
+        }
     }
 
     public List<FranchiseDto> getFranchiseList() {
