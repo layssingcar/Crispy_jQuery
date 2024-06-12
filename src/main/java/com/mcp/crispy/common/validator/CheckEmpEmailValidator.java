62cefa34c6574e674454010b4a0a4eec9a4e0425
Baemint2 <qofndlwl@gmail.com>
2024-06-12 09:02:47 +0900

    아이디 중복 검증 직원 등록 / 점주 등록 분기 처리

--- src/main/java/com/mcp/crispy/common/validator/CheckEmpEmailValidator.java
+++ src/main/java/com/mcp/crispy/common/validator/CheckEmpEmailValidator.java
@@ -1,6 +1,7 @@
 package com.mcp.crispy.common.validator;
 
 import com.mcp.crispy.employee.dto.EmployeeRegisterDto;
+import com.mcp.crispy.employee.dto.OwnerRegisterDto;
 import com.mcp.crispy.employee.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -8,14 +9,22 @@ import org.springframework.validation.Errors;
 
 @Component
 @RequiredArgsConstructor
-public class CheckEmpEmailValidator  extends AbstractValidator<EmployeeRegisterDto>{
+public class CheckEmpEmailValidator<T> extends AbstractValidator<T>{
 
     private final EmployeeService employeeService;
 
     @Override
-    protected void doValidate(EmployeeRegisterDto employeeRegisterDto, Errors errors) {
-        if (employeeService.existsByEmail(employeeRegisterDto.getEmpEmail())) {
-            errors.rejectValue("empEmail", "empEmail", "이미 사용중인 이메일입니다.");
+    protected void doValidate(T target, Errors errors) {
+        if (target instanceof EmployeeRegisterDto employeeRegisterDto) {
+            if (employeeService.existsByEmail(employeeRegisterDto.getEmpEmail())) {
+                errors.rejectValue("empEmail", "empEmail", "이미 사용중인 이메일입니다.");
+            }
+        } else {
+            if (target instanceof OwnerRegisterDto ownerRegisterDto) {
+                if (employeeService.existsByEmail(ownerRegisterDto.getEmpEmail())) {
+                    errors.rejectValue("empEmail", "empEmail", "이미 사용중인 이메일입니다.");
+                }
+            }
         }
     }
 }
--- src/main/java/com/mcp/crispy/common/validator/ValidationService.java
+++ src/main/java/com/mcp/crispy/common/validator/ValidationService.java
@@ -1,8 +1,11 @@
 package com.mcp.crispy.common.validator;
 
+import com.mcp.crispy.common.exception.CustomValidationException;
 import com.mcp.crispy.employee.dto.EmployeeRegisterDto;
+import com.mcp.crispy.employee.dto.OwnerRegisterDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
 
 @Service
@@ -12,8 +15,23 @@ public class ValidationService {
     private final CheckEmpIdValidator checkEmpIdValidator;
     private final CheckEmpEmailValidator checkEmpEmailValidator;
 
-    public void validateEmployee(EmployeeRegisterDto employeeRegisterDto, BindingResult bindingResult) {
+    public void validateEmployee(EmployeeRegisterDto employeeRegisterDto) {
+        BindingResult bindingResult = new BeanPropertyBindingResult(employeeRegisterDto, "employeeRegisterDto");
         checkEmpIdValidator.validate(employeeRegisterDto, bindingResult);
         checkEmpEmailValidator.validate(employeeRegisterDto, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            throw new CustomValidationException(bindingResult);
+        }
     }
+
+    public void validateOwner(OwnerRegisterDto ownerRegisterDto) {
+        BindingResult bindingResult = new BeanPropertyBindingResult(ownerRegisterDto, "ownerRegisterDto");
+        checkEmpIdValidator.validate(ownerRegisterDto, bindingResult);
+        checkEmpEmailValidator.validate(ownerRegisterDto, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            throw new CustomValidationException(bindingResult);
+        }
+    };
 }
