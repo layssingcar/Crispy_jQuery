d1bcf4b439a7a3c6169d06651a8ab62f5b2642a3
Baemint2 <qofndlwl@gmail.com>
2024-06-16 13:08:04 +0900

    로그인 예외 로직, 스타일 수정

--- src/main/java/com/mcp/crispy/auth/domain/LoginRequest.java
+++ src/main/java/com/mcp/crispy/auth/domain/LoginRequest.java
@@ -1,9 +1,12 @@
 package com.mcp.crispy.auth.domain;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
+    @NotBlank(message = "아이디를 입력해주세요")
     private String username;
+    @NotBlank(message = "비밀번호를 입력해주세요")
     private String password;
 }
