93fec6aeb7c493cef4d6fe1a6235eb73960035e1
layssingcar <69200364+layssingcar@users.noreply.github.com>
2024-09-09 14:38:09 +0900

    Update approvalList.js 1차 완료 (진행 중)

--- src/main/resources/static/js/approval/approvalList.js
+++ src/main/resources/static/js/approval/approvalList.js
@@ -9,50 +9,48 @@ const optionObj = {
 
 // 페이지 이동
 const addPageLinkEventFn = pageNo => {
-    const pageLinks = document.querySelectorAll(".page-link"); // 페이지 버튼
-
-    pageLinks.forEach(pageLink => {
-        pageLink.addEventListener("click", e => {
+    $(".page-link").each(function() {
+        $(this).on("click", e => {
             e.preventDefault(); // a 태그 기본 동작 방지
 
-            const pageNo = pageLink.dataset.pageNo;
+            const pageNo = $(this).data("pageNo");
             optionObj["pageNo"] = pageNo;
             getApprItemsFn(optionObj);
-        })
+        });
 
         // active 클래스 추가
-        if (Number(pageLink.innerText) === Number(pageNo))
-            pageLink.parentElement.classList.add("active");
-    })
+        if (Number($(this).text()) === Number(pageNo))
+            $(this).parent().addClass("active");
+    });
 }
 
 // 기안자 검색
-document.querySelector("#search")?.addEventListener("input", e => {
-    optionObj["empName"] = e.target.value;
+$("#search").on("input", e => {
+    optionObj["empName"] = $(e.target).val();
     optionObj["pageNo"] = 1;
     getApprItemsFn(optionObj);
-})
+});
 
 // 카테고리 구분 조회
-document.querySelector("#time-off-ct-no").addEventListener("change", e => {
-    optionObj["timeOffCtNo"] = e.target.value;
+$("#time-off-ct-no").on("change", e => {
+    optionObj["timeOffCtNo"] = $(e.target).val();
     optionObj["pageNo"] = 1;
     getApprItemsFn(optionObj);
-})
+});
 
 // 문서상태 구분 조회
-document.querySelector("#appr-stat").addEventListener("change", e => {
-    optionObj["apprStat"] = e.target.value;
+$("#appr-stat").on("change", e => {
+    optionObj["apprStat"] = $(e.target).val();
     optionObj["pageNo"] = 1;
     getApprItemsFn(optionObj);
-})
+});
 
 // 기안일 정렬
 const addSortEventFn = () => {
-    document.querySelector("#appr-dt-sort").addEventListener("click", e => {
+    $("#appr-dt-sort").on("click", () => {
         optionObj["apprDtSort"] = (optionObj["apprDtSort"] === "DESC") ? "ASC" : "DESC";
         getApprItemsFn(optionObj);
-    })
+    });
 }
 
 // 결재 문서 항목 리스트
@@ -60,7 +58,7 @@ const getApprItemsFn = async (optionObj) => {
     const params = new URLSearchParams(); // URL 쿼리 문자열 객체
 
     for (let key in optionObj)
-        params.append(key, optionObj[key])
+        params.append(key, optionObj[key]);
 
     // URL 경로에서 type 변수 값을 얻어와 저장
     const subIdx = location.pathname.lastIndexOf("/");
@@ -68,7 +66,7 @@ const getApprItemsFn = async (optionObj) => {
 
     const response = await fetch(`/crispy/approval-items/${type}?${params.toString()}`);
     const html = await response.text();
-    document.querySelector(".appr-list-container").outerHTML = html;
+    $(".appr-list-container").html(html);
 
     // 이벤트 재추가
     addPageLinkEventFn(optionObj.pageNo === undefined ? 1 : optionObj.pageNo);
@@ -77,20 +75,18 @@ const getApprItemsFn = async (optionObj) => {
 }
 
 // 초기화
-document.addEventListener("DOMContentLoaded", function () {
+$(() => {
     addPageLinkEventFn(1);
     addSortEventFn();
     addApprRowsEventFn();
-})
+});
 
 // 결재 문서 항목
 const addApprRowsEventFn = () => {
-    const apprRows = document.querySelectorAll(".appr-row");
-
-    apprRows.forEach(apprRow => {
-        apprRow.addEventListener("click", () => {
-            const apprNo = apprRow.dataset.apprNo;
+    $(".appr-row").each(function() {
+        $(this).on("click", () => {
+            const apprNo = $(this).data("apprNo");
             location.href = `/crispy/approval-detail/time-off/${apprNo}`;
-        })
-    })
+        });
+    });
 }
